b3b7f25e5acc7e0d53f81dfb124e85c0c8c4b976
bw1611 <bw1611@naver.com>
2023-05-22 17:42:17 +0900

    init commit

---
+++ src/main/java/com/ll/FlexGym/FlexGymApplication.java
@@ -0,0 +1,13 @@
+package com.ll.FlexGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/FlexGym/FlexGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.FlexGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
