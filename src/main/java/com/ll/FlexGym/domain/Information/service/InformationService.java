07a474798448265c4125cb662414dc4b75a90941
tkdtn4657 <tkdtn101226@gmail.com>
2023-06-07 22:16:25 +0900

    feat: database에 content, favoriteId를 제외한 값 추가

--- src/main/java/com/ll/FlexGym/domain/Information/service/InformationService.java
+++ src/main/java/com/ll/FlexGym/domain/Information/service/InformationService.java
@@ -1,5 +1,9 @@
 package com.ll.FlexGym.domain.Information.service;
 
+import com.ll.FlexGym.domain.Information.entity.Information;
+import com.ll.FlexGym.domain.Information.repository.InformationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,8 +13,12 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class InformationService {
 
+    private final InformationRepository informationRepository;
     @Transactional
     public void setYoutubeData(HashMap youtubeData){
 //        String videoId;
@@ -32,20 +40,41 @@ public class InformationService {
         }
         String[] titleAndThumnailurl = new String[10];
         ArrayList<String> titleAndThumnailurl2 = new ArrayList<>();
+        ArrayList<String> titleArray = new ArrayList<>();
+        ArrayList<String> videoThumnailUrlArray = new ArrayList<>();
         for(int i = 0; i < sl.size(); i++) {
             titleAndThumnailurl = sl.get(i).split("=");
             titleAndThumnailurl[0] = titleAndThumnailurl[0].substring(1, titleAndThumnailurl[0].length());
             titleAndThumnailurl[1] = titleAndThumnailurl[1].substring(0, titleAndThumnailurl[1].length()-1);
-            titleAndThumnailurl2.add(titleAndThumnailurl[0]);
-            titleAndThumnailurl2.add(titleAndThumnailurl[1]);
+            titleArray.add(titleAndThumnailurl[0]);
+            videoThumnailUrlArray.add(titleAndThumnailurl[1]);
         }
+
         String[] s = hs.keySet().toArray(new String[0]);
         for (String ssss: s){
-            System.out.println(ssss);
+//            System.out.println(ssss);
+            log.info("ssss = {}", ssss);
         }
         for(String sss : titleAndThumnailurl2){
-            System.out.println(sss);
+//            System.out.println(sss);
+            log.info("sss = {}", sss);
         }
+
+//        private String content;
+//
+//        private String videoId;
+//        private String title;
+//        private String videoThumnailUrl;
+        for(int i = 0; i < 5; i++) {
+            Information information = Information
+                    .builder()
+                    .videoId(s[i])
+                    .title(titleArray.get(i))
+                    .videoThumnailUrl(videoThumnailUrlArray.get(i))
+                    .build();
+            informationRepository.save(information);
+        }
+
     }
 
 }
