210120b73b4f55063a09f8ed3f271f6afcce804f
seol11796 <seol11796@gmail.com>
2023-06-08 11:18:23 +0900

    categroy bug fixed

--- src/main/java/com/ll/FlexGym/domain/Board/entity/Category.java
+++
@@ -1,5 +0,0 @@
-package com.ll.FlexGym.domain.Board.entity;
-
-public enum Category {
-    운동일지, 상체운동, 하체운동, 바디프로필, 식단, 단백질, 유산소
-}
--- src/main/java/com/ll/FlexGym/global/init/NotProd.java
+++ src/main/java/com/ll/FlexGym/global/init/NotProd.java
@@ -27,20 +27,20 @@ public class NotProd {
             @Override
             @Transactional
             public void run(String... args) throws Exception {
-                Member memberUser1 = memberService.join("user1", "1234").getData();
-                Member memberUser2 = memberService.join("user2", "1234").getData();
-                Member memberUser3 = memberService.join("user3", "1234").getData();
+//                Member memberUser1 = memberService.join("user1", "1234").getData();
+//                Member memberUser2 = memberService.join("user2", "1234").getData();
+//                Member memberUser3 = memberService.join("user3", "1234").getData();
 
-                ChatRoom chatRoom1 = chatRoomService.createAndSave("환영합니다.1", memberUser1.getId());
-                ChatRoom chatRoom2 = chatRoomService.createAndSave("환영합니다.2", memberUser2.getId());
-
-                Meeting meeting1 = meetingService.create("오늘 한강에서 러닝하실 분 구합니다.",
-                        memberUser1, 8, "여의도", "2023-07-08 14:00",
-                        "한강에서 2시간 정도 같이 달리실 분 구합니다!");
-
-                Meeting meeting2 = meetingService.create("이번주 토요일 바이크 타실 분 구합니다.",
-                        memberUser2,4, "마포구", "2023-06-28 10:00",
-                        "오전에 같이 운동해요!");
+//                ChatRoom chatRoom1 = chatRoomService.createAndSave("환영합니다.1", memberUser1.getId());
+//                ChatRoom chatRoom2 = chatRoomService.createAndSave("환영합니다.2", memberUser2.getId());
+//
+//                Meeting meeting1 = meetingService.create("오늘 한강에서 러닝하실 분 구합니다.",
+//                        memberUser1, 8, "여의도", "2023-07-08 14:00",
+//                        "한강에서 2시간 정도 같이 달리실 분 구합니다!");
+//
+//                Meeting meeting2 = meetingService.create("이번주 토요일 바이크 타실 분 구합니다.",
+//                        memberUser2,4, "마포구", "2023-06-28 10:00",
+//                        "오전에 같이 운동해요!");
             }
         };
     }
