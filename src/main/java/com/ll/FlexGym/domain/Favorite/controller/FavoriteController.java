05d303bca6902cb06732180f2062a1876c04e7e4
tkdtn4657 <tkdtn101226@gmail.com>
2023-06-13 09:26:40 +0900

    Design: 테두리 적용 및 radius를 활용, 해 카드에 맞는 이미지 출력

---
+++ src/main/java/com/ll/FlexGym/domain/Favorite/controller/FavoriteController.java
@@ -0,0 +1,4 @@
+package com.ll.FlexGym.domain.Favorite.controller;
+
+public class FavoriteController {
+}
---
+++ src/main/java/com/ll/FlexGym/domain/Favorite/service/FavoriteService.java
@@ -0,0 +1,4 @@
+package com.ll.FlexGym.domain.Favorite.service;
+
+public class FavoriteService {
+}
