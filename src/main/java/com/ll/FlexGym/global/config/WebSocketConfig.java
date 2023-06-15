ea37b9d8cee2131dbadadc0f273d97661a62d9b8
bw1611 <bw1611@naver.com>
2023-06-15 16:34:09 +0900

    bugFIx

--- src/main/java/com/ll/FlexGym/global/config/WebSocketConfig.java
+++ src/main/java/com/ll/FlexGym/global/config/WebSocketConfig.java
@@ -1,10 +1,17 @@
 package com.ll.FlexGym.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.security.Principal;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,15 +20,28 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic"); // 메시지브로커를 등록하는 코드
-        config.setApplicationDestinationPrefixes("/app"); // 도착 경로에 대한 prefix를 설정
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")   //SockJS 연결 주소
+                .setAllowedOriginPatterns("*")
                 .setAllowedOrigins("http://localhost:8080")
                 .setAllowedOrigins("https://www.colikeprdo.work")// spring stomp CORS 설정하기
-                .withSockJS(); //버전 낮은 브라우저에서도 적용 가능
+                .setHandshakeHandler(new DefaultHandshakeHandler() {
+                    @Override
+                    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+                        Principal principal = request.getPrincipal();
+                        if (principal != null) {
+                            // WebSocket 세션에 인증 정보를 넣어둡니다.
+                            attributes.put("PRINCIPAL", principal);
+                        }
+                        return principal;
+                    }
+                })
+                .withSockJS()
+                .setInterceptors(new HttpSessionHandshakeInterceptor());
         // 주소 : ws://localhost:8080/ws
     }
 }
